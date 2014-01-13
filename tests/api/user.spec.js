e464df2d0283a8015f2681d7c2149b8e166a223f
Valeri Bogdanov <valeribogdanov@gmail.com>
2014-01-13 17:31:02 +0200

    bdd, test is changed so that each it is independent

--- tests/api/user.spec.js
+++ tests/api/user.spec.js
@@ -2,7 +2,12 @@ describe("user api", function(){
   var request = require("request");
   var helpers = require("../helpers");
 
-  it("starts", helpers.startApiHttpServer);
+  beforeEach(function (next) {
+    helpers.startApiHttpServer(next);
+  });
+  afterEach(function (next) {
+    helpers.stopApiHttpServer(next);
+  });
 
   it("registers new user", function(next){
     request.post({
@@ -18,32 +23,55 @@ describe("user api", function(){
     });
   });
 
-  it("logout user", function(next){
-    request.get({
-      uri: helpers.apiendpoint+"/users/me/logout",
-      json: {}
-    }, function(err, res, body){
-      expect(res.statusCode).toBe(200);
-      next();
+  describe("user registered and logged in", function () {
+    beforeEach(function (next) {
+      request.post({
+        uri: helpers.apiendpoint+"/users/register",
+        json: {
+          username: "testuser",
+          password: "test"
+        }
+      }, function(err, res, body){
+        next();
+      });
     });
-  });
 
-  it("login user", function(next){
-    request.get({
-      uri: helpers.apiendpoint+"/users/me/login",
-      json: {
-        username: "testuser",
-        password: "test"
-      }
-    }, function(err, res, body){
-      expect(res.statusCode).toBe(200);
-      expect(body.result._id).toBeDefined();
-      expect(body.result.username).toBe("testuser");
-      expect(body.result.password).not.toBeDefined();
-      next();
+    it("logout user", function(next){
+      request.get({
+        uri: helpers.apiendpoint+"/users/me/logout",
+        json: {}
+      }, function(err, res, body){
+        expect(res.statusCode).toBe(200);
+        next();
+      });
     });
-  });
 
-  it("stops", helpers.stopApiHttpServer);
-  
+    describe("and logged out", function () {
+      beforeEach(function (next) {
+        request.get({
+          uri: helpers.apiendpoint+"/users/me/logout",
+          json: {}
+        }, function(err, res, body){
+          next();
+        });
+      });
+
+      it("login user", function(next){
+        request.get({
+          uri: helpers.apiendpoint+"/users/me/login",
+          json: {
+            username: "testuser",
+            password: "test"
+          }
+        }, function(err, res, body){
+          expect(res.statusCode).toBe(200);
+          expect(body.result._id).toBeDefined();
+          expect(body.result.username).toBe("testuser");
+          expect(body.result.password).not.toBeDefined();
+          next();
+        });
+      });    
+    });
+
+  });
 });
