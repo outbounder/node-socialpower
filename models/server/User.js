e8987153325356b5ea085d28bca1ae624d62d401
Boris Filipov <obiwonn@gmail.com>
2014-01-14 13:13:02 +0200

    polished impl of send message and improved test

--- models/server/User.js
+++ models/server/User.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var crypto = require("crypto");
+var _ = require("underscore")
 
 var Message = require("./Message")
 
@@ -38,25 +39,44 @@ schema.static("findOneByUsernamePassword", function(username, password, callback
   this.findOne(pattern).exec(callback);
 })
 
+var drawPoints = function(sender, msg, callback) {
+  // 2. use msg._id to find users who send the message before
+  // 2.1. increment those users points
+  User.update({ messages: msg }, { $inc: { points: 1 } }, function(err, users){
+    if(err) return callback(err)
+
+    // only if sender didn't send the message before 
+    if(!sender.hasMessage(msg)) {
+      // 3. add the message._id to current user's messages and increment user's points
+      sender.points += 1
+      sender.messages.push(msg)
+      sender.save(callback)
+    } else
+      callback()
+  })
+}
+
+schema.method("hasMessage", function(msg){
+  _.contains(this.messages, function(item){
+    return item.toString() === msg._id.toString()
+  })
+})
+
 schema.method("sendMessage", function(message, callback){
   var self = this
   // 1. check is message already stored in DB
   Message.findOne(message, function(err, msg){
+    if(err) return callback(err)
+
     // 1.1 create the message if not stored yet
-    if(!msg) msg = new Message()
-
-    // 2. use msg._id to find users who send the message before
-    User.find({ messages: msg._id }, function(err, users){
-      // 2.1. increment those users points
-      users.forEach(function(user){
-        user.points += 1
-        user.save(console.error)
+    if(!msg) {
+      msg = new Message(message)
+      msg.save(function(err){
+        if(err) return callback(err)
+        drawPoints(self, msg, callback)
       })
-
-      // 3. add the message._id to current user's messages
-      self.points += 1
-      self.save(callback)
-    })
+    } else
+      drawPoints(self, msg, callback)
   })
 })
 
--- tests/api/message.spec.js
+++ tests/api/message.spec.js
@@ -11,6 +11,46 @@ describe("user api", function(){
       }
     }, function(err, res, body){
       expect(res.statusCode).toBe(200)
+      expect(body.points).toBe(1)
+      next()
+    })
+  })
+
+  it("user sends message again", function(next){
+    request.post({
+      uri: helpers.apiendpoint+"/messages",
+      json: {
+        body: "a test message"
+      }
+    }, function(err, res, body){
+      expect(res.statusCode).toBe(200)
+      expect(body.points).toBe(2)
+      next()
+    })
+  })
+
+  it("user sends message third time", function(next){
+    request.post({
+      uri: helpers.apiendpoint+"/messages",
+      json: {
+        body: "a test message"
+      }
+    }, function(err, res, body){
+      expect(res.statusCode).toBe(200)
+      expect(body.points).toBe(3)
+      next()
+    })
+  })
+
+  it("user sends another message", function(next){
+    request.post({
+      uri: helpers.apiendpoint+"/messages",
+      json: {
+        body: "a test message 2"
+      }
+    }, function(err, res, body){
+      expect(res.statusCode).toBe(200)
+      expect(body.points).toBe(4)
       next()
     })
   })
